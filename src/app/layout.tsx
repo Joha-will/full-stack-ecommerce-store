44f74c5c64d7298122c7c662bbf8010edecca58a
Joha-will <johawilsonatu@icloud.com>
2024-07-05 12:13:03 +0100

    created Nav component with Navlink and created admin page and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-        "bg-background min-h-screen font-sans antialiased", 
-        inter.variable
-        )}>
-          {children}
+      <body
+        className={cn(
+          "bg-background min-h-screen font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
       </body>
     </html>
   );
