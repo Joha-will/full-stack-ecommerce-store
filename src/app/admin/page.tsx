fb06a15f0808791bd900472f9990bf7efccd19dd
Joha-will <johawilsonatu@icloud.com>
2024-07-07 22:10:28 +0100

    created DashboardCard component and added settings to config database to make queries

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,37 @@
-import { Card, CardHeader } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 
 
 export default function AdminDashboard() {
     return (
         <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 gap-4">
-            <Card>
-                <CardHeader> Sales </CardHeader>
-            </Card>
+
+            <DashboardCard title='Sales' description='Descript' body='body' />
+            
         </div>
     )
+}
+
+type DashboardCardProps = {
+    title: string;
+    description: string;
+    body: string;
+}
+
+function DashboardCard({title, description, body}: DashboardCardProps) {
+
+    return (
+
+        <Card>
+            <CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+            <p>{body}</p>
+            </CardContent>
+
+        </Card>
+
+    );
 }
