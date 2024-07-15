3a17e8ff77d6380884eb09cb43dc76beec5966e7
Joha-will <johawilsonatu@icloud.com>
2024-07-15 23:47:00 +0100

    installed shadcn Button component and added Tailwindcss style to admin products page

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,10 +1,19 @@
 import { PageHeader } from "../_components/PageHeader"
+import { Button } from "@/components/ui/button"
 
 
 export default function AdminProductsPage() {
     return (
         <>
-            <PageHeader> Products </PageHeader> 
+            <div className="flex justify-between items-center gap-4">
+                <PageHeader> Products </PageHeader>
+                <Button asChild>
+                    <link href="/admin/products/new">Add Product</link>
+                </Button>
+
+            </div>
+             
+
         </>
     )
 }
