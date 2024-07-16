328353cb4a99ed8f1687c4ca6038dac3fe2261e9
Joha-will <johawilsonatu@icloud.com>
2024-07-16 22:11:18 +0100

    installed shadcn table component and create basic productTable function

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,5 +1,6 @@
 import { PageHeader } from "../_components/PageHeader"
 import { Button } from "@/components/ui/button"
+import { Table } from "@/components/ui/table"
 
 
 export default function AdminProductsPage() {
@@ -12,8 +13,22 @@ export default function AdminProductsPage() {
                 </Button>
 
             </div>
+            <div>
+                {/* Product List */}
+                <h2>List of Products</h2>
+            </div>
              
 
         </>
+    )
+}
+
+function ProductTable() {
+    return (
+        <Table>
+
+            
+        </Table>
+
     )
 }
