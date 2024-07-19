8b2d3d49326465b11d67a3fe31a0444e9e4581cc
Joha-will <johawilsonatu@icloud.com>
2024-07-19 23:43:34 +0100

    added more validation functionality to addProduct function

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,10 +1,12 @@
-import { z } from 'zod' 
+import { z } from 'zod';
+import db from '@/db/db';
+import fs from 'fs/promises';
 
 const fileSchema = z.instanceof(File, { message: 'Required'})
 const imageSchema = fileSchema.refine( file => file.size === 0 || file.type.startsWith("image/"))
 
 
-z.object({
+const addSchema = z.object({
     name: z.string().min(1),
     description: z.string().min(1),
     priceInCents: z.coerce.number().int().min(1),
@@ -14,7 +16,21 @@ z.object({
 })
 
 export async function addProduct(formData: FormData) {
-    
-    return 
+
+    const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+
+    if (result.success === false) {
+        return result.error.formErrors.fieldErrors;
+    }
+
+    const data = result.data
+
+    db.product.create({
+        data: {
+            name: data.name,
+            description: data.description,
+            priceInCents: data.priceInCents,
+        },
+    })
 
 }
