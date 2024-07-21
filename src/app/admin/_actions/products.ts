91b7937453adda7cde3a6112fab8372a730ef8b4
Joha-will <johawilsonatu@icloud.com>
2024-07-21 22:09:48 +0100

    added functionality to addProduct function for creating a project

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,6 +1,9 @@
+'use server'
+
 import { z } from 'zod';
 import db from '@/db/db';
 import fs from 'fs/promises';
+import { redirect } from 'next/navigation';
 
 const fileSchema = z.instanceof(File, { message: 'Required'})
 const imageSchema = fileSchema.refine( file => file.size === 0 || file.type.startsWith("image/"))
@@ -25,12 +28,27 @@ export async function addProduct(formData: FormData) {
 
     const data = result.data
 
-    db.product.create({
+    await fs.mkdir('products', { recursive: true });
+    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+
+
+    await fs.mkdir('public/products', { recursive: true });
+    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+    await fs.writeFile(`public{imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+
+    
+
+    await db.product.create({
         data: {
             name: data.name,
             description: data.description,
             priceInCents: data.priceInCents,
+            filePath,
+            imagePath,
         },
-    })
+    });
+
+    redirect('admin/products')
 
 }
